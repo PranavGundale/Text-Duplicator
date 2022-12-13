81a64b81d5d0416f11cb29e79adbd3a05a401c0e
PranavGundale <109735316+PranavGundale@users.noreply.github.com>
2022-12-13 21:15:18 +0530

    Update sourcecode.py

--- sourcecode.py
+++ sourcecode.py
@@ -1,6 +1,6 @@
 file = open("MyData.txt", "r") 
 file2 = open("MyDataDuplicated.txt", "w")
 file2.write(file.read()) #copying the text from file1 to paste it on file2
-file2.close() #closing the dialouge box
+file2.close() #closing the dialogue box
 
 # Pranav Gundale
